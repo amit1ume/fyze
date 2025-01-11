20a28547e4d3951f29bb185b68968ac11dcbaf01
Nikhil Gupta <nikhil@Nikhils-MacBook-Pro.local>
2025-01-12 01:12:45 +0530

    Improved Logging

---
+++ src/main/java/com/samartha/fyze/common/constants/LogConstants.java
@@ -0,0 +1,37 @@
+package com.samartha.fyze.common.constants;
+
+public class LogConstants {
+
+	public static class MdcKeys {
+
+		public static final String REQUEST_ID = "requestId";
+
+		public static final String STOCK_ID = "stockId";
+
+		public static final String ADVISOR_ID = "advisorId";
+
+		public static final String RECOMMENDATION_ID = "recommendationId";
+
+	}
+
+	public static class KVPFields {
+
+		public static final String METHOD = "method";
+
+		public static final String URL = "url";
+
+		public static final String REQUEST_PARAMS = "requestParams";
+
+		public static final String REQUEST_HEADERS = "requestHeaders";
+
+		public static final String REQUEST_BODY = "requestBody";
+
+		public static final String RESPONSE_STATUS = "responseStatus";
+
+		public static final String RESPONSE_BODY = "responseBody";
+
+		public static final String DURATION = "duration";
+
+	}
+
+}
